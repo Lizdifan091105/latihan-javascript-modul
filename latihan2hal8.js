74e8730f235739b7ac06eba293149c2083b17f87
ahmad nafidzil ala <162394204+Lizdifan091105@users.noreply.github.com>
2024-04-07 21:03:59 +0700

    latihanmoduljs

---
+++ latihan2hal8.js
@@ -0,0 +1,4 @@
+// ini digunakan ketika komentar berupa satu baris
+/* ini digunakan ketika komentar berupa lebih satu baris
+komentar merupakan bagian yanng tidak dijalankan oleh program*/
+alert ('halo')
---
+++ latihan3hal11.js
@@ -0,0 +1,14 @@
+//program memnanfaatkan beberapa jenis varaibel yaitu menghitung volume tabung
+
+var diameter =12
+let tinggi  =13
+/*let dan var bisa dirubah nilainya seperti ini*/
+diameter=20
+tinggi=23
+//sedangkan const ttidak bisa dirubah setelah di isi
+const phi=3.14
+
+let nama =phi*diameter*tinggi
+
+console.log(nama)
+//output 1444.4
